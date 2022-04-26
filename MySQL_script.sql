ffc88c50a4a76f6ab23ade5480a148c5fa5bc204
Icaro-tme <52382988+Icaro-tme@users.noreply.github.com>
2022-04-26 00:31:22 -0300

    inserted data, fixed foreign keys & columns

--- MySQL_script.sql
+++ MySQL_script.sql
@@ -1,5 +1,6 @@
 
-/*Criar usuario e dar todos os privilégios para todas as operações*/
+
+/*Criar usuario e dar dotos os privilégios para todas as operações*/
 CREATE USER 'icaro'@'localhost' IDENTIFIED BY '123123';
 
 GRANT ALL PRIVILEGES ON *.* TO 'icaro'@'localhost';
@@ -50,7 +51,7 @@ CREATE TABLE funcionario (
                 nome_meio CHAR(1),
                 ultimo_nome VARCHAR(15) NOT NULL,
                 data_nascimento DATE,
-                endereco VARCHAR(30) NOT NULL,
+                endereco VARCHAR(50) NOT NULL,
                 sexo CHAR(1) NOT NULL,
                 salario DECIMAL(10,2),
                 cpf_supervisor CHAR(11) NOT NULL,
@@ -70,7 +71,7 @@ ALTER TABLE funcionario MODIFY COLUMN ultimo_nome VARCHAR(15) COMMENT 'Sobrenome
 
 ALTER TABLE funcionario MODIFY COLUMN data_nascimento DATE COMMENT 'Data de nascimento do funcionario.';
 
-ALTER TABLE funcionario MODIFY COLUMN endereco VARCHAR(30) COMMENT 'Endereço do funcionário.';
+ALTER TABLE funcionario MODIFY COLUMN endereco VARCHAR(50) COMMENT 'Endereço do funcionário.';
 
 ALTER TABLE funcionario MODIFY COLUMN sexo CHAR(1) COMMENT 'sexo do funcionario.';
 
@@ -140,61 +141,129 @@ ALTER TABLE trabalha_em MODIFY COLUMN horas DECIMAL(3, 1) COMMENT 'Horas trabalh
 /*criar tabela localizacoes_departamento & comentarios*/
 CREATE TABLE localizacoes_departamento (
                 numero_departamento INT NOT NULL,
-                local_ VARCHAR(15) NOT NULL,
-                PRIMARY KEY (numero_departamento, local_)
+                local VARCHAR(15) NOT NULL,
+                PRIMARY KEY (numero_departamento, local)
 );
 
 ALTER TABLE localizacoes_departamento COMMENT 'Tabela que armazena as possíveis localizações dos departamentos.';
 
 ALTER TABLE localizacoes_departamento MODIFY COLUMN numero_departamento INTEGER COMMENT 'm é uma FK para a tabela departamento.';
 
-ALTER TABLE localizacoes_departamento MODIFY COLUMN local_ VARCHAR(15) COMMENT 'Localização do departamento. Faz parte da PK desta tabela.';
+ALTER TABLE localizacoes_departamento MODIFY COLUMN local VARCHAR(15) COMMENT 'Localização do departamento. Faz parte da PK desta tabela.';
 
 /*----- Adicionando Constraints/Criando relções entre tabelas com foreign keys ----- */
-ALTER TABLE localizacoes_departamento ADD CONSTRAINT departamento_localizacoes_departamento__fk
+
+ALTER TABLE localizacoes_departamento ADD CONSTRAINT departamento_localizacoes_departamento_fk
 FOREIGN KEY (numero_departamento)
 REFERENCES departamento (numero_departamento)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE projeto ADD CONSTRAINT departamento_projeto__fk
+ALTER TABLE projeto ADD CONSTRAINT departamento_projeto_fk
 FOREIGN KEY (numero_departamento)
 REFERENCES departamento (numero_departamento)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE funcionario ADD CONSTRAINT departamento_funcionario__fk
-FOREIGN KEY (numero_departamento)
-REFERENCES departamento (numero_departamento)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE funcionario ADD CONSTRAINT funcionario__funcionario__fk1
-FOREIGN KEY (cpf_supervisor)
-REFERENCES funcionario (cpf)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE dependente ADD CONSTRAINT funcionario__dependente_fk
-FOREIGN KEY (cpf_funcionario)
-REFERENCES funcionario (cpf)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE departamento ADD CONSTRAINT funcionario__departamento_fk
+ALTER TABLE departamento ADD CONSTRAINT funcionario_departamento_fk
 FOREIGN KEY (cpf_gerente)
 REFERENCES funcionario (cpf)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE trabalha_em ADD CONSTRAINT funcionario__trabalha_em__fk
+ALTER TABLE funcionario ADD CONSTRAINT funcionario_funcionario_fk
+FOREIGN KEY (cpf_supervisor)
+REFERENCES funcionario (cpf)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE dependente ADD CONSTRAINT funcionario_dependente_fk
 FOREIGN KEY (cpf_funcionario)
 REFERENCES funcionario (cpf)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE trabalha_em ADD CONSTRAINT projeto__trabalha_em__fk
+ALTER TABLE trabalha_em ADD CONSTRAINT funcionario_trabalha_em_fk
+FOREIGN KEY (cpf_funcionario)
+REFERENCES funcionario (cpf)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE trabalha_em ADD CONSTRAINT projeto_trabalha_em_fk
 FOREIGN KEY (numero_projeto)
 REFERENCES projeto (numero_projeto)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
+
+/*----------------INSERTS DE DADOS--------------------*/
+
+--# Insert da tabela funcionarios
+--# Jorge precisa ser inserido primeiro pois o CPF dele é uma foreign key de outros funcionarios
+INSERT INTO funcionario(primeiro_nome, nome_meio, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
+VALUES('Jorge', 'E', 'Brito', '88866555576', '1937-11-10','Rua do Horto, 35, São Paulo, SP', 'M', '55000', '88866555576', 1 ),
+('Jennifer', 'S', 'Souza', '98765432168', '1941-06-20','Av.Arthur de Lima, 54, Santo André, SP', 'F', '43000', '88866555576', 4 ),
+('Alice', 'J', 'Zelaya', '99988777767', '1968-01-19','Rua Souza Lima, 35, Curitiba, PR', 'F', '25000', '98765432168', 4 ),
+('André', 'V', 'Pereira', '98798798733', '1969-03-29','Rua Timbira, 35, São Paulo, SP', 'M', '25000', '98765432168', 4 ),
+('Fernando', 'T', 'Wong', '33344555587', '1955-12-08','Rua da Lapa, 34, São Paulo, SP', 'M', '40000', '88866555576', 5 ),
+('João', 'B', 'Silva', '12345678966', '1965-01-09','Rua das Flores, 751, São Paulo, SP', 'M', '30000', '33344555587', 5 ),
+('Ronaldo', 'K', 'Lima', '66688444476', '1962-09-15','Rua Rebouças, 65, Piracicaba, SP', 'M', '38000', '33344555587', 5 ),
+('Joice', 'A', 'Leite', '45345345376', '1972-07-31','Av. Lucas Obes, 74, São Paulo, SP', 'F', '25000', '33344555587', 5 );
+
+--# Insert da tabela departamento		
+INSERT INTO departamento (nome_departamento, numero_departamento, cpf_gerente, data_inicio_gerente)
+VALUES ('Pesquisa', 5, 33344555587, '1988-05-22'),
+('Administração', 4, 98765432168, '1995-01-01'),
+('Matriz', 1, 88866555576, '1981-06-19');
+
+--# Insert da tabela localizacoes_departamento
+INSERT INTO localizacoes_departamento (numero_departamento, local) 	 
+VALUES (1, 'São Paulo'),
+(4, 'Mauá'),
+(5, 'Santo André'),
+(5, 'Itu'),
+(5, 'São Paulo');
+
+--# Insert da tabela projeto
+INSERT INTO projeto (nome_projeto, numero_projeto, local_projeto, numero_departamento)
+VALUES ('ProdutoX', 1, 'Santo André', 5),
+('ProdutoY', 2, 'Itu', 5),
+('ProdutoZ', 3, 'São Paulo', 5),
+('Informatização', 10,'Mauá', 4),
+('Reogarnização', 20, 'São Paulo', 1),
+('Novosbenefícios', 30, 'Mauá', 4);
+
+--# Insert da tabela dependente
+INSERT INTO dependente (cpf_funcionario, nome_dependente, sexo, data_nascimento, parentesco)
+VALUES (33344555587, 'Alicia', 'F', '1986-04-05', 'Filha'),
+(33344555587, 'Tiago', 'M', '1983-10-25', 'Filho'),
+(33344555587, 'Janaína', 'F', '1958-05-03', 'Esposa'),
+(98765432168, 'Antonio', 'M', '1942-02-28', 'Marido'),
+(12345678966, 'Michael', 'M', '1988-01-04', 'Filho'),
+(12345678966, 'Alicia', 'F', '1988-12-30', 'Filha'),
+(12345678966, 'Elizabeth', 'F', '1967-05-05', 'Esposa');
+
+--# Insert da tabela trabalha_em
+insert into trabalha_em (cpf_funcionario, numero_projeto, horas)
+values (12345678966, 1, 32.5),
+(12345678966, 2, 7.5),
+(66688444476, 3, 40.0),
+(45345345376, 1, 20.0),
+(45345345376, 2, 10.0),
+(33344555587, 2, 10.0),
+(33344555587, 3, 10.0),
+(33344555587, 10, 10.0),
+(33344555587, 20, 10.0),
+(99988777767, 30, 30.0),
+(99988777767, 10, 10.0),
+(98798798733, 10, 35.0),
+(98798798733, 30, 5.0),
+(98765432168, 30, 20.0),
+(98765432168, 20, 15.0),
+(88866555576, 20, 0);
+
+-->Fim dos inserts
+
+
+
+
+
